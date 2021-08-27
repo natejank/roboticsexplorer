7e006aa0e40f3d597c05555e3149050754d4f33c
sloshyyy <nate@jankowskis.net>
2021-08-27 01:05:19 -0400

    Added GPL headers to all main source files
    Good enough for me; GPL applies whether or not the header exists.

--- app/src/main/java/net/example/apstudent/roboticsexplorer/tbarequest/TBAJsonArrayRequest.java
+++ app/src/main/java/net/example/apstudent/roboticsexplorer/tbarequest/TBAJsonArrayRequest.java
@@ -1,3 +1,20 @@
+/* Child class of JsonArrayRequest to make it easier to make requests to TBA for JSON Arrays.
+ *     Copyright (C) 2021  Nathan Jankowski
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package net.example.apstudent.roboticsexplorer.tbarequest;
 
 import androidx.annotation.Nullable;
